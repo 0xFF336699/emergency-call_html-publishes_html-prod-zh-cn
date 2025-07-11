9fbe2db66a09bdf36470dad433ea558e4ba5dc84
0xFF336699 <0xFF336699@gmail.com>
2025-07-11 20:05:16 +0800

    Fri Jul 11 20:05:15     2025

---
+++ docs/_next/static/chunks/pages/settings-a7c8efacabc598d6.js
@@ -0,0 +1,78 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[199],{
+
+/***/ 4080:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/settings",
+      function () {
+        return __webpack_require__(9379);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ }),
+
+/***/ 9379:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(4513);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(4285);
+/* harmony import */ var _barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5216);
+/* harmony import */ var _barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5814);
+/* harmony import */ var _barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(7917);
+/* harmony import */ var _barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(3655);
+/* harmony import */ var _barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(2050);
+
+
+
+const SettingsPage = ()=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .A, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .A, {
+                position: "static",
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .A, {
+                    children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .A, {
+                        variant: "h6",
+                        component: "div",
+                        sx: {
+                            flexGrow: 1
+                        },
+                        children: "Settings"
+                    })
+                })
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .A, {
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .A, {
+                    sx: {
+                        my: 2
+                    },
+                    children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_AppBar_Box_Container_Toolbar_Typography_mui_material__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .A, {
+                        children: "Here you can configure the application."
+                    })
+                })
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SettingsPage);
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [653,636,593,792], () => (__webpack_exec__(4080)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
+//# sourceMappingURL=settings-a7c8efacabc598d6.js.map
