6e615a4440e78e7c80e9bc93faa1566a8ab6e037
0xFF336699 <0xFF336699@gmail.com>
2025-08-15 15:30:46 +0800

    Fri Aug 15 15:30:46     2025

---
+++ docs/_next/static/chunks/pages/prod-idea-be78ee6e0e449795.js
@@ -0,0 +1,82 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[4977],{
+
+/***/ 51668:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/prod-idea",
+      function () {
+        return __webpack_require__(63837);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ }),
+
+/***/ 63837:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ ProdIdea)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(94513);
+/* harmony import */ var _barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6445);
+/* harmony import */ var _barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9220);
+
+
+function CityAgent() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A, {
+                children: "广告收益代理商"
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A, {
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A, {
+                        children: "预售各个城市或区县的广告收益，也可以是他自己投放广告，可能收取的钱偏少，但是具体需要讨论后再定。"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A, {
+                        children: "这部分钱类似于众筹创业募集资金的方式。未来如果成了，投资者会收益很丰厚。"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A, {
+                children: "给我们支持的合作伙伴也可以赠与一些这样的条件，例如如果拿下北京的红十字、保安、地铁等，我们可以给未来几年的收益。"
+            })
+        ]
+    });
+}
+function Security() {
+    return /*#__PURE__*/ _jsxs(Box, {
+        children: [
+            /*#__PURE__*/ _jsx(Box, {
+                children: "纳入保安和红十字系统"
+            }),
+            /*#__PURE__*/ _jsx(Box, {})
+        ]
+    });
+}
+function ProdIdea() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A, {
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_Box_Stack_mui_material__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .A, {
+            children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(CityAgent, {})
+        })
+    });
+}
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [636,6593,8792], () => (__webpack_exec__(51668)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
+//# sourceMappingURL=prod-idea-be78ee6e0e449795.js.map
